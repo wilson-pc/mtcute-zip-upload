9cfa3d0cf3f90c5eba2593dd30beb4a50b170cf0
Wilver Hidalgo <wilver.hidalgo@believesol.com>
2025-04-13 15:59:36 -0400

    first commit

---
+++ index.ts
@@ -0,0 +1,35 @@
+import { BotKeyboard, Message, TelegramClient, md } from '@mtcute/bun'
+import { Dispatcher, filters } from '@mtcute/dispatcher'
+
+const apiId = Number(process.env.API_ID)
+const apiHash = process.env.API_HASH
+const token = process.env.BOT_KEY || ''
+
+const tg = new TelegramClient({
+  apiId: apiId,
+  apiHash: apiHash ?? ''
+})
+
+;(async () => {
+  await tg.start({
+    botToken: token
+  })
+
+  const dp = Dispatcher.for(tg)
+  dp.onNewMessage(filters.text, async (msg) => {
+    const userId = msg?.sender?.id
+    console.log(userId)
+    const chatId = msg.chat.id
+     const filePath = `./archives.zip`
+    const media = await tg.uploadFile({
+        file: filePath,
+        fileMime: 'application/zip',
+      })
+      await tg.sendMedia(msg.chat.id, {
+        file: media,
+        type: 'document',
+        fileMime: 'application/zip',
+        fileName: `archives.zip`
+      }) 
+  })
+})()
